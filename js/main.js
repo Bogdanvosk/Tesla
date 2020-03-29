05d381b47f93c0a2217677bc959fcd7db04560a5
Bogdan Voskanov <bogdanvoskanov@gmail.com>
2020-03-29 23:26:33 +0300

    Animations added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,55 @@
-$(document).ready(function () {
-	// ! Slider
-	$('.slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		dots: true,
-		fade: true
-	})
+$(document).ready(function() {
+    // ! Slider
+    $('.slider').slick({
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        fade: true
+    })
 
 
-	// ! Menu Navigation
-	const menuBtn = document.querySelector('.header-btn'),
-			menuBlock = document.querySelector('.menu'),
-			sliderDots = document.querySelector('.slick-dots'),
-			closeBtn = document.querySelector('.close-btn');
+    // ! Menu Navigation
+    const menuBtn = document.querySelector('.header-btn'),
+        menuBlock = document.querySelector('.menu'),
+        sliderDots = document.querySelector('.slick-dots'),
+        closeBtn = document.querySelector('.close-btn');
 
-	menuBtn.addEventListener('click', () => {
-		menuBlock.classList.add('active');
-	})
+    menuBtn.addEventListener('click', () => {
+        menuBlock.classList.add('active');
+    })
 
-	closeBtn.addEventListener('click', () => {
-		menuBlock.classList.remove('active');
-	})
+    closeBtn.addEventListener('click', () => {
+        menuBlock.classList.remove('active');
+    })
+
+
+
+    $('.header-content__title').addClass('hidden').viewportChecker({
+        classToAdd: 'visibility animated fadeInLeft',
+        offset: 100
+    });
+
+    $('.header-content__text').addClass('hidden').viewportChecker({
+        classToAdd: 'visibility animated fadeInRight delay-1s',
+        offset: 100
+    });
+
+    $('.stats-item').addClass('hidden').viewportChecker({
+        classToAdd: 'visibility animated bounceIn',
+        offset: 100
+    });
+
+    $('.benefits-row').addClass('hidden').viewportChecker({
+        classToAdd: 'visibility animated fadeInRight',
+        offset: 100
+    });
+
+    $('.design-row__item').addClass('hidden').viewportChecker({
+        classToAdd: 'visibility animated zoomIn',
+        offset: 100
+    });
+
 
 });
