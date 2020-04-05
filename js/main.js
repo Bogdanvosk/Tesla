2612bb8d6b6cec9ef50750f74bd91487d683a2a6
Bogdan Voskanov <bogdanvoskanov@gmail.com>
2020-04-05 18:51:06 +0300

    Preloader added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+    // Preloader
+    $('.loader-inner').loaders();
+
+    document.body.onload = function() {
+
+        setTimeout(function() {
+            let preloader = document.getElementById('page-preloader');
+            if (!preloader.classList.contains('done')) {
+                preloader.classList.add('done');
+            }
+        }, 1000)
+
+    }
+
     // ! Slider
     $('.slider').slick({
         infinite: true,
